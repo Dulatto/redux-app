3977b342c403b4da6d9b3cdaaee864fe0d65a30d
Dulatto <dulat.omarov@gmail.com>
2021-04-27 22:52:07 -0400

    redux

---
+++ src/components/pages/home-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//import BookList from '../book-list';
+
+const HomePage = () => {
+    return (
+        <div>Home Page</div>
+        // <BookList />
+    );
+};
+
+export default HomePage;
