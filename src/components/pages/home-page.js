c5e2aa13d3057f0099039e0113a8ee89ab44d7b3
Dulatto <dulat.omarov@gmail.com>
2021-04-27 23:28:30 -0400

    routing

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,10 +1,10 @@
 import React from 'react';
-//import BookList from '../book-list';
+import BookList from '../book-list';
 
 const HomePage = () => {
     return (
-        <div>Home Page</div>
-        // <BookList />
+
+        <BookList />
     );
 };
 
