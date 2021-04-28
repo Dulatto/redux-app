1e83c154c886dad951bac0060612d1957411ca12
Dulatto <dulat.omarov@gmail.com>
2021-04-27 23:50:27 -0400

    cart

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ShopHeader from '../shop-header';
 import { CartPage, HomePage } from '../pages';
 import './app.css';
 
@@ -7,10 +8,13 @@ import './app.css';
 const App = () => {
 
     return (
-        <Switch>
-            <Route path='/' component={HomePage} exact />
-            <Route path='/cart' component={CartPage} />
-        </Switch>
+        <main role="main" className="container">
+            <ShopHeader bumItem={5} total={210} />
+            <Switch>
+                <Route path='/' component={HomePage} exact />
+                <Route path='/cart' component={CartPage} />
+            </Switch>
+        </main>
     )
 };
 
