668462cd4c255cc263db7ad1344d2ab7cb10555e
Dulatto <dulat.omarov@gmail.com>
2021-04-28 23:58:46 -0400

    error

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -19,9 +19,14 @@ export default class BookstoreService {
     ];
 
     getBooks() {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
-                resolve(this.data)
+                if (Math.random() > 0.75) {
+                    reject(new Error('Somthing bad happened'))
+                } else {
+                    resolve(this.data);
+                }
+
             }, 700);
         })
     }
