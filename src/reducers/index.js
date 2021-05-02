f5170a3bf739b9d6e3906ac5ff5d306da4d7f15e
Dulatto <dulat.omarov@gmail.com>
2021-05-02 00:07:24 -0400

    cart

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,6 +10,14 @@ const initialState = {
 }
 
 const updateCartItems = (cartItems, item, idx) => {
+
+    if (item.count === 0) {
+        return [
+            ...cartItems.slice(0, idx),
+            ...cartItems.slice(idx + 1)
+        ]
+    }
+
     if (idx === -1) {
         return [
             ...cartItems,
@@ -23,16 +31,32 @@ const updateCartItems = (cartItems, item, idx) => {
     ];
 };
 
-const updateCartItem = (book, item = {}) => {
+const updateCartItem = (book, item = {}, quantity) => {
     const { id = book.id, count = 0, title = book.title, total = 0 } = item;
 
     return {
         id,
         title,
-        count: count + 1,
-        total: total + book.price
+        count: count + quantity,
+        total: total + quantity * book.price
     }
 };
+
+const updateOrder = (state, bookId, quantity) => {
+
+    const { books, cartItems } = state;
+
+    const book = books.find(({ id }) => id === bookId);
+    const itemIndex = cartItems.findIndex(({ id }) => id === bookId);
+    const item = cartItems[itemIndex];
+
+    const newItem = updateCartItem(book, item, quantity);
+
+    return {
+        ...state,
+        cartItems: updateCartItems(cartItems, newItem, itemIndex)
+    };
+}
 
 const reducer = (state = initialState, action) => {
 
@@ -61,17 +85,11 @@ const reducer = (state = initialState, action) => {
             };
 
         case 'BOOK_ADDED_TO_CART':
-            const bookId = action.payload;
-            const book = state.books.find((book) => book.id === bookId);
-            const itemIndex = state.cartItems.findIndex(({ id }) => id === bookId);
-            const item = state.cartItems[itemIndex];
+            return updateOrder(state, action.payload, 1);
+        case 'BOOK_REMOVED_FROM_CART':
+            return updateOrder(state, action.payload, -1);
+        case 'ALL_BOOKS_REMOVED_FROM_CART':
 
-            const newItem = updateCartItem(book, item);
-
-            return {
-                ...state,
-                cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
-            };
 
         default:
             return state;
