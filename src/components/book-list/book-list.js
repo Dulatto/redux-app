c64b27908ec38890ae856ce44147fe4ec5d5d14f
Dulatto <dulat.omarov@gmail.com>
2021-05-01 22:41:08 -0400

    cart

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,13 +10,17 @@ import './book-list.css';
 import Spinner from '../spinner/spinner';
 import ErrorIndicator from '../error-indicator';
 
-const BookList = ({ books }) => {
+const BookList = ({ books, onAddedToCart }) => {
     return (
         <ul className="book-list">
             {
                 books.map((book) => {
                     return (
-                        <li key={book.id}><BookListItem book={book} /></li>
+                        <li key={book.id}>
+                            <BookListItem
+                                book={book}
+                                onAddedToCart={() => onAddedToCart(book.id)} />
+                        </li>
                     )
                 })
             }
@@ -32,7 +36,7 @@ class BookListContainer extends Component {
     }
 
     render() {
-        const { books, loading, error } = this.props;
+        const { books, loading, error, onAddedToCart } = this.props;
 
         if (loading) {
             return <Spinner />;
@@ -42,7 +46,7 @@ class BookListContainer extends Component {
             return <ErrorIndicator />;
         }
 
-        return <BookList books={books} />
+        return <BookList books={books} onAddedToCart={onAddedToCart} />
 
     }
 }
@@ -54,7 +58,10 @@ const mapStateToProps = ({ books, loading, error }) => {
 const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 
     return {
-        fetchBooks: fetchBooks(bookstoreService, dispatch)
+        fetchBooks: fetchBooks(bookstoreService, dispatch),
+        onAddedToCart: (id) => {
+            console.log('Added to cart', id);
+        }
     }
 };
 
