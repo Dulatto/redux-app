d20be853e7cf6cfd126e028ad45e8f27a9ea20a3
Dulatto <dulat.omarov@gmail.com>
2021-05-02 23:04:13 -0400

    final

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+
 const booksRequested = () => {
     return {
         type: 'FETCH_BOOKS_REQUEST'
     };
 };
+
 const booksLoaded = (newBooks) => {
     return {
         type: 'FETCH_BOOKS_SUCCESS',
@@ -16,26 +18,27 @@ const booksError = (error) => {
         payload: error
     };
 };
+
 export const bookAddedToCart = (bookId) => {
     return {
         type: 'BOOK_ADDED_TO_CART',
         payload: bookId
-    }
-}
+    };
+};
 
 export const bookRemovedFromCart = (bookId) => {
     return {
         type: 'BOOK_REMOVED_FROM_CART',
         payload: bookId
-    }
-}
+    };
+};
 
 export const allBooksRemovedFromCart = (bookId) => {
     return {
         type: 'ALL_BOOKS_REMOVED_FROM_CART',
         payload: bookId
-    }
-}
+    };
+};
 
 const fetchBooks = (bookstoreService, dispatch) => () => {
     dispatch(booksRequested());
